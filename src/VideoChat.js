d10a6fa43a3e1f3a52ad833e973132c7f443ddd7
katherynbelxy <36305427+katherynbelxy@users.noreply.github.com>
2024-10-22 10:51:00 -0500

    cambio ruta

--- src/VideoChat.js
+++ src/VideoChat.js
@@ -5,7 +5,7 @@ import io from 'socket.io-client';
 
 // const socket = io.connect('http://localhost:5000');
 const socket = io("wss://video-chat-backend-theta.vercel.app", {
-  transports: ["websocket", "polling"],
+  transports: ["polling"],
   secure: true
 });
 
